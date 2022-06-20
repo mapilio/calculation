b70b0ef278ebdd19e480b9898fda25e6a27d8e84
mcv_ <murat@visiosoft.com.tr>
2022-06-20 15:07:02 +0300

    area heron fixed

--- calculation/area.py
+++ calculation/area.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Area:
 
     # Deprecated
@@ -6,3 +9,22 @@ class Area:
                          interSection_B_lat, interSection_B_lon,
                          interSection_C_lat, interSection_C_lon):
         pass
+
+    @staticmethod
+    def calc_area_heron(a: float, b: float, c: float) -> float:
+        """
+             A _______ B
+            |    X    |
+            D _______ C
+
+        Args:
+            a: triangle edge ABC
+            b: triangle edge BCD
+            c: triangle edge CAD
+
+        Returns:
+            area float
+        """
+
+        s = (a + b + c) / 2
+        return math.sqrt(abs(s * (s - a) * (s - b) * (s - c)))
