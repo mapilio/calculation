7a7c14b8863a88a96a1e251722317b3e3ba38f64
Kadir Coşar <kadircosarnew@gmail.com>
2022-07-19 15:48:15 +0300

    __init__.py import lines for upper directory converted to comment line.

--- calculation/__init__.py
+++ calculation/__init__.py
@@ -6,10 +6,10 @@ Calculation makes it easy for mapilio to provide accessibility for data sources.
 Contains all above functions. Needs improvement on destination point and angle calculation. Rest works fine
 """
 
-from .area import Area
-from .distance import Distance
-from .intersection import Intersection
-from .iterator import Iterator
-from .pixel import Pixel
 
-from .util import __version__, __major__, __minor__, __patch__
+# from .area import Area
+# from .distance import Distance
+# from .intersection import Intersection
+# from .iterator import Iterator
+#
+# from .util import __version__, __major__, __minor__, __patch__
