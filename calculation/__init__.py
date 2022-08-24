686f0ba9c55db13f278a7cbd2e243e4ff9b4efdb
Kadir Coşar <kadircosarnew@gmail.com>
2022-08-24 11:30:11 +0300

    README.md update

--- calculation/__init__.py
+++ calculation/__init__.py
@@ -5,11 +5,3 @@ Calculation makes it easy for mapilio to provide accessibility for data sources.
 
 Contains all above functions. Needs improvement on destination point and angle calculation. Rest works fine
 """
-
-
-# from .area import Area
-# from .distance import Distance
-# from .intersection import Intersection
-# from .iterator import Iterator
-#
-# from .util import __version__, __major__, __minor__, __patch__
