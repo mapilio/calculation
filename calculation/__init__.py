fbed1168cef606a8371c303aed9be2e1d1ee9ec0
Kadir Coşar <kadircosarnew@gmail.com>
2022-08-25 12:54:57 +0300

    solve conflicts

--- calculation/__init__.py
+++ calculation/__init__.py
@@ -5,3 +5,4 @@ Calculation makes it easy for mapilio to provide accessibility for data sources.
 
 Contains all above functions. Needs improvement on destination point and angle calculation. Rest works fine
 """
+
