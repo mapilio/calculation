c54ca05c9d0e0c584dde839538fe9a2a62e46f56
Görkem Gül <104680758+gorkemgul@users.noreply.github.com>
2024-01-09 17:41:39 +0300

    update: add moved functions from core as util.py
    These are the mandatory functions for calculation which were refactored and moved from core.

---
+++ util.py
@@ -0,0 +1,40 @@
+import math
+
+def get_aspect_ratio(width, height):
+
+    """
+    Calculates the aspect ratio from given width and height.
+
+    Args:
+         width (int): width in pixels
+         height (int): height in pixels
+
+    Returns:
+         Calculated aspect ratio
+
+    """
+
+    aspect_ratio = width / height if width > height else height / width
+
+    return aspect_ratio
+
+
+def calculate_vfov(fov, width, height):
+    """
+    Calculates vfov from given width, height and hvof.
+
+    Args:
+        hfov (float): horizontal fov in degrees
+        width (int): width in pixels
+        height (int): height in pixels
+
+    Returns:
+        Calculated vertical fov in degrees.
+
+    """
+    hfov = math.radians(fov)
+    vfov = 2 * math.atan(
+        math.tan(hfov / 2) / get_aspect_ratio(width, height))
+    vfov = math.degrees(vfov)
+
+    return vfov
